2492982085445b7afb379252e51a357c8bd86fec
Peterson Alves <peterrson047@gmail.com>
2023-07-03 08:45:21 -0300

    inicio documentação
    adicionado comentários explicando código no script de index.php e functions.php

--- functions.php
+++ functions.php
@@ -1,78 +1,89 @@
 <?php
 
-define('VIEW','392093547');
-define('SERVICE_ACCOUNT','pwa-temmais-6d342bd03c26.json');
-define('DOMAIN','http://www.ubunters.blogspot.com');
+// Define constantes
+define('VIEW', '263429314');
+define('SERVICE_ACCOUNT', 'pwa-temmais-6d342bd03c26.json');
+define('DOMAIN', 'http://172.16.145.50/temmais/aracatuba');
 
-
-function getActivePages($analytics){
+// Obtém as páginas ativas no momento
+function getActivePages($analytics)
+{
   $optParams = array(
     'dimensions' => 'rt:pageTitle,rt:pagePath',
     'sort' => '-rt:activeVisitors',
     'max-results' => '3'
   );
   $result = $analytics
-        ->data_realtime
-        ->get('ga:'.VIEW, 'rt:activeVisitors',$optParams);
+    ->data_realtime
+    ->get('ga:' . VIEW, 'rt:activeVisitors', $optParams);
 
   $table = '';
-  if($result){
+  if ($result) {
     $rows = $result->getRows();
-    if($rows){
-      foreach($rows as $row){
-        $table .= '<tr class="open-link" data-link="'.$row[1].'">';
-        $table .= '<td>'.htmlspecialchars($row[0],ENT_NOQUOTES).'</td>';
-        $table .= '<td>'.htmlspecialchars($row[2],ENT_NOQUOTES).'</td>';
+    if ($rows) {
+      foreach ($rows as $row) {
+        // Constrói uma linha da tabela HTML com o título e o caminho da página
+        $table .= '<tr class="open-link" data-link="' . $row[1] . '">';
+        $table .= '<td>' . htmlspecialchars($row[0], ENT_NOQUOTES) . '</td>';
+        $table .= '<td>' . htmlspecialchars($row[2], ENT_NOQUOTES) . '</td>';
         $table .= '</tr>';
       }
-    }else{
+    } else {
+      // Mensagem exibida quando não há dados para exibir
       $table .= '<tr><td colspan="2"><small>There is no data to view</small></td></tr>';
     }
     return $table;
-  }else{
+  } else {
+    // Mensagem exibida quando não há dados para exibir
     return '<tr><td colspan="2"><small>There is no data to view</small></td></tr>';
   }
 }
 
-function getActiveUsers($analytics){
+// Obtém o número de usuários ativos no momento
+function getActiveUsers($analytics)
+{
   $active_users = $analytics
-          ->data_realtime
-          ->get('ga:'.VIEW, 'rt:activeVisitors');
-  $active_users = (isset($active_users->rows[0][0]))?$active_users->rows[0][0]:0;
+    ->data_realtime
+    ->get('ga:' . VIEW, 'rt:activeVisitors');
+  $active_users = (isset($active_users->rows[0][0])) ? $active_users->rows[0][0] : 0;
   return $active_users;
 }
 
-function getDevices($analytics){
+// Obtém informações sobre os dispositivos utilizados pelos usuários ativos no momento
+function getDevices($analytics)
+{
   $optParams = array(
     'dimensions' => 'rt:deviceCategory',
     'sort' => '-rt:activeVisitors'
   );
 
   $devices = $analytics
-        ->data_realtime
-        ->get('ga:'.VIEW, 'rt:activeVisitors',$optParams);
+    ->data_realtime
+    ->get('ga:' . VIEW, 'rt:activeVisitors', $optParams);
   $html = '';
-  if($devices->rows){
+  if ($devices->rows) {
     $total = 0;
-    $class = array('warning','success','danger');
-    foreach($devices->rows as $row){
+    $class = array('warning', 'success', 'danger');
+    foreach ($devices->rows as $row) {
       $total += $row[1];
     }
     $loop = 0;
     $html .= '<div class="progress_label">';
-    foreach($devices->rows as $row){
-      $percent = round(($row[1]/$total)*100);
-      $html .= '<div class="label label-'.$class[$loop].'">';
-      $html .= '<span>'.$row[0].'</span>';
-      $html .= '<span>'.$row[1].'</span>';
+    foreach ($devices->rows as $row) {
+      $percent = round(($row[1] / $total) * 100);
+      // Constrói uma representação HTML de barras de progresso com as porcentagens de uso de cada dispositivo
+      $html .= '<div class="label label-' . $class[$loop] . '">';
+      $html .= '<span>' . $row[0] . '</span>';
+      $html .= '<span>' . $row[1] . '</span>';
       $html .= '</div>';
       $loop++;
     }
     $html .= '</div>';
     $loop = 0;
-    $html .= '<div class="progress" style="width:100%!important">';
-    foreach($devices->rows as $row){
-      $html .= '<div class="progress-bar progress-bar-'.$class[$loop].'" style="width:'.$percent.'%"></div>';
+    $html .= '
+    <div class="progress" style="width:100%!important">';
+    foreach ($devices->rows as $row) {
+      $html .= '<div class="progress-bar progress-bar-' . $class[$loop] . '" style="width:' . $percent . '%"></div>';
       $loop++;
     }
     $html .= '</div>';
@@ -80,29 +91,34 @@ function getDevices($analytics){
   return $html;
 }
 
-
-function getFormattedData($result){
+// Formata os dados obtidos em uma tabela HTML
+function getFormattedData($result)
+{
   $table = '';
-  if($result){
+  if ($result) {
     $rows = $result->getRows();
-    if($rows){
-      foreach($rows as $row){
+    if ($rows) {
+      foreach ($rows as $row) {
         $table .= '<tr>';
-        foreach($row as $cell){
-          $table .= '<td>'.htmlspecialchars($cell,ENT_NOQUOTES).'</td>';
+        foreach ($row as $cell) {
+          $table .= '<td>' . htmlspecialchars($cell, ENT_NOQUOTES) . '</td>';
         }
         $table .= '</tr>';
       }
-    }else{
+    } else {
+      // Mensagem exibida quando não há dados para exibir
       $table .= '<tr><td colspan="2"><small>There is no data to view</small></td></tr>';
     }
     return $table;
-  }else{
+  } else {
+    // Mensagem exibida quando não há dados para exibir
     return '<tr><td colspan="2"><small>There is no data to view</small></td></tr>';
   }
 }
 
-function getTrafficSources($analytics){
+// Obtém as fontes de tráfego ativas no momento
+function getTrafficSources($analytics)
+{
   $optParams = array(
     'dimensions' => 'rt:source',
     'sort' => '-rt:activeVisitors',
@@ -110,12 +126,14 @@ function getTrafficSources($analytics){
   );
 
   $result = $analytics
-        ->data_realtime
-        ->get('ga:'.VIEW,'rt:activeVisitors',$optParams);
+    ->data_realtime
+    ->get('ga:' . VIEW, 'rt:activeVisitors', $optParams);
   return getFormattedData($result);
 }
 
-function getCountries($analytics){
+// Obtém os países dos usuários ativos no momento
+function getCountries($analytics)
+{
   $optParams = array(
     'dimensions' => 'ga:country',
     'sort' => '-rt:activeVisitors',
@@ -123,13 +141,14 @@ function getCountries($analytics){
   );
 
   $result = $analytics
-        ->data_realtime
-        ->get('ga:'.VIEW,'rt:activeVisitors',$optParams);
-  return getFormattedData($result,'Country','Users');
+    ->data_realtime
+    ->get('ga:' . VIEW, 'rt:activeVisitors', $optParams);
+  return getFormattedData($result, 'Country', 'Users');
 }
 
-
-function getOS($analytics){
+// Obtém os sistemas operacionais dos usuários ativos no momento
+function getOS($analytics)
+{
   $optParams = array(
     'dimensions' => 'ga:operatingSystem',
     'sort' => '-rt:activeVisitors',
@@ -137,13 +156,14 @@ function getOS($analytics){
   );
 
   $result = $analytics
-        ->data_realtime
-        ->get('ga:'.VIEW,'rt:activeVisitors',$optParams);
-  return getFormattedData($result,'OS','Users');
+    ->data_realtime
+    ->get('ga:' . VIEW, 'rt:activeVisitors', $optParams);
+  return getFormattedData($result, 'OS', 'Users');
 }
 
-
-function getBrowser($analytics){
+// Obtém os navegadores utilizados pelos usuários ativos no momento
+function getBrowser($analytics)
+{
   $optParams = array(
     'dimensions' => 'ga:browser',
     'sort' => '-rt:activeVisitors',
@@ -151,7 +171,7 @@ function getBrowser($analytics){
   );
 
   $result = $analytics
-        ->data_realtime
-        ->get('ga:'.VIEW,'rt:activeVisitors',$optParams);
-  return getFormattedData($result,'Browser','Users');
+    ->data_realtime
+    ->get('ga:' . VIEW, 'rt:activeVisitors', $optParams);
+  return getFormattedData($result, 'Browser', 'Users');
 }
